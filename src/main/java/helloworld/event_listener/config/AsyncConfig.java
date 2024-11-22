e66d79615ac3fab9f2f041e5d1c68eacd15bb68c
june-777 <wlwhswnsrl96@gmail.com>
2024-11-22 16:09:04 +0900

    feat: 비동기 환경 설정

---
+++ src/main/java/helloworld/event_listener/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package helloworld.event_listener.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
